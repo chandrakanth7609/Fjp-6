41545c5bda5e3954a2d7a1f4b92f5668adbfb529
chandu-chandrakanth <chandusirusala@gmail.com>
2022-07-18 11:16:42 +0530

    inroduction if javascript

---
+++ intro.js
@@ -0,0 +1,26 @@
+console.log("welcome to java script");
+console.log("chandu chadukora");
+//variable
+let a=10;
+console.log(a);
+let char="kiran";
+console.log(char);
+let str="these two are brothers";
+console.log(str);
+let bool=true;
+console.log(bool);
+//loops
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+for(let i=1;i<=10;i++){
+
+console.log(i);
+}
+let count=10;
+while(count>-5){
+    console.log(count);
+    count--;
+}
