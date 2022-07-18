66e36556d7c8b2b30e0e7b10bc1952db24e80b52
chandu-chandrakanth <chandusirusala@gmail.com>
2022-07-18 16:55:33 +0530

    introduction of javascript

---
+++ function.js
@@ -0,0 +1,25 @@
+//functions withiut parameter and return type
+
+function sayhello(){
+    console.log("Hello from functions");
+}
+sayhello();
+
+//function with parameter
+function sum(num1,num2){
+    let addition=num1+num2
+        console.log("addition of two numbers:"+addition)
+}
+sum(10,30)
+
+//function with return type
+function multiply(num1,num2){
+    return num1*num2;
+}
+console.log("multiplication of two numbers:"+multiply(5,10))
+
+//functions are the first class citizen in java script
+let a=function sub(num1,num2){
+    return num1-num2;
+}
+console.log(a(10,2))
