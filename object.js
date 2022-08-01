ea39e6502be0d31a5c9983e0bab8b05556f375d0
chandu-chandrakanth <chandusirusala@gmail.com>
2022-08-01 19:19:00 +0530

    object functions concept

--- object.js
+++ object.js
@@ -12,3 +12,27 @@ let objChandu={
 
 };
 console.log(objChandu);
+let capAmerica={
+    Name:"chandu",
+    "Age":"22",
+    friends:["hari","praveen","kapil"],
+    phoneNo:7702180472,
+    address:{                       //nested object
+        city:"shabad",
+        state:"Telangana"
+    },
+    sayHi:function(){
+        console.log("cap america says hii");
+    }
+}
+console.log(capAmerica);
+console.log(capAmerica.Age);
+console.log(capAmerica.Name);
+console.log(capAmerica.address);
+console.log(capAmerica.phoneNo);
+console.log(capAmerica.friends);
+console.log(capAmerica.sayHi);
+
+//call from say hii
+capAmerica.sayHi();                         function call
+console.log(capAmerica.address.state);
