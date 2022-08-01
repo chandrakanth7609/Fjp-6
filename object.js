7b60588d0a8d396a6440feaae0fe36c967d2341c
chandu-chandrakanth <chandusirusala@gmail.com>
2022-08-01 19:01:37 +0530

    object creation

---
+++ object.js
@@ -0,0 +1,14 @@
+// empty object
+
+let obj={};
+console.log(obj);
+
+//create  a object
+let objChandu={
+    Name:"chandu",
+    "Age":"22",
+    "phoneNo":"7702180472",
+    lastname : "sirusala"
+
+};
+console.log(objChandu);
